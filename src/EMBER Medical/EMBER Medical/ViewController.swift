022a2a368b33aad3ad27487858dee350ebd1fe1e
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-01-14 18:48:03 +0200

    Init

---
+++ src/EMBER Medical/EMBER Medical/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EMBER Medical
+//
+//  Created by Ahmed Moussa on 1/14/20.
+//  Copyright © 2020 Ahmed Moussa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
