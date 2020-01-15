8efe97acd9ca92f70b6606e72c82061580a202b7
Ahmed Moussa <moussa.ahmed95@gmail.com>
2020-01-15 19:47:13 +0200

    Small Changes
    - Made Obfuscator functions public so I can use it in API Manager

--- src/Common/APIManager/APIManager/Base/APIManagerConfig.swift
+++ src/Common/APIManager/APIManager/Base/APIManagerConfig.swift
@@ -7,9 +7,12 @@
 //
 
 import Foundation
+import Obfuscator
 
 public class APIManagerConfig {
     public static var serverUrl: URL? {
-        return nil
+        let key: [UInt8] = [39, 22, 18, 5, 0, 89, 78, 91, 1, 23, 57, 32, 46, 18, 3, 75, 12, 6, 40, 77, 16, 71, 92]
+        let obf = Obfuscator()
+        return URL(string: obf.reveal(key: key))
     }
 }
